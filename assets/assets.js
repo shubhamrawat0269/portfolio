04aa3323127a296e67a133c9bc864cb09ecfca30
Shubham Rawat <rawatshubam269@gmail.com>
2025-06-07 17:23:47 +0530

    updated : assets changes

--- assets/assets.js
+++ assets/assets.js
@@ -65,29 +65,6 @@ export const assets = {
   right_arrow_bold_dark,
 };
 
-export const workData = [
-  {
-    title: "Frontend project",
-    description: "Web Design",
-    bgImage: "/work-1.png",
-  },
-  {
-    title: "Geo based app",
-    description: "Mobile App",
-    bgImage: "/work-2.png",
-  },
-  {
-    title: "Photography site",
-    description: "Web Design",
-    bgImage: "/work-3.png",
-  },
-  {
-    title: "UI/UX designing",
-    description: "UI/UX Design",
-    bgImage: "/work-4.png",
-  },
-];
-
 export const cardData = [
   {
     icon: <Code size={40} />,
