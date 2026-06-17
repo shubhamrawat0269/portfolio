38254b47dbe807037b32648b9f0509c862915d77
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-17 12:04:19 +0530

    Updated : smooth animation for hero, github activity

--- app/components/organisms/GithubActivity.jsx
+++ app/components/organisms/GithubActivity.jsx
@@ -1,5 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
 
 const GitHubCalendar = dynamic(
   () => import("react-github-calendar").then((mod) => mod.GitHubCalendar),
@@ -8,9 +9,17 @@ const GitHubCalendar = dynamic(
 
 export default function GithubActivity() {
   return (
-    <section className="max-w-screen-xl mx-auto p-3 flex flex-col items-center pb-16">
-      <h2 className="text-[#b1b1b1] py-3 text-lg">GitHub Activity</h2>
-      <GitHubCalendar username="gaearon" />
+    <section className="max-w-screen-xl mx-auto p-3 pb-16">
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ type: "spring", stiffness: 320, damping: 70, mass: 1 }}
+        className="flex flex-col items-center justify-center gap-4 py-10"
+      >
+        <h2 className="text-[#b1b1b1] py-3 text-lg">GitHub Activity</h2>
+        <GitHubCalendar username="gaearon" />
+      </motion.div>
     </section>
   );
 }
--- app/components/organisms/HeroSection.jsx
+++ app/components/organisms/HeroSection.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { FileText, Mail } from "lucide-react";
-import { assets, containerVariants, fadeUp, scaleIn } from "@/assets/assets";
+import { assets, containerVariants } from "@/assets/assets";
 
 export default function HeroSection() {
   return (
@@ -14,7 +14,18 @@ export default function HeroSection() {
         className="text-center max-w-screen-lg"
       >
         {/* Profile Image */}
-        <motion.div variants={scaleIn}>
+        <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 0.2,
+            type: "spring",
+            stiffness: 320,
+            damping: 70,
+            mass: 1,
+          }}
+        >
           <Image
             src={assets.profile_img}
             alt="Shubham Rawat"
@@ -25,7 +36,12 @@ export default function HeroSection() {
         </motion.div>
 
         {/* Text Content */}
-        <motion.div variants={fadeUp}>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", stiffness: 240, damping: 70, mass: 1 }}
+        >
           <h1 className="text-4xl sm:text-5xl lg:text-7xl flex flex-col gap-2 text-[#bcbaba] py-4 font-bold">
             <span>
               Hi, I'm <span className="text-[#a78bfa]">Shubham</span>
@@ -34,7 +50,18 @@ export default function HeroSection() {
           </h1>
         </motion.div>
 
-        <motion.div variants={fadeUp}>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 0.2,
+            type: "spring",
+            stiffness: 320,
+            damping: 70,
+            mass: 1,
+          }}
+        >
           <h2 className="text-xl font-bold text-[#bcbaba] py-2">
             Building intuitive UIs and AI-driven tools with a sharp eye for
             craft.
@@ -43,7 +70,10 @@ export default function HeroSection() {
 
         {/* Buttons */}
         <motion.div
-          variants={fadeUp}
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ type: "spring", stiffness: 320, damping: 70, mass: 1 }}
           className="flex flex-col md:flex-row items-center justify-center gap-4 py-10"
         >
           <a href="#contact">
